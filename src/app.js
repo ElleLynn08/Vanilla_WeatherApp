c64608b8429043d0678494863488f6b07501542b
MFindley08 <123705549+MFindley08@users.noreply.github.com>
2023-04-06 23:49:57 -0400

    added current time

--- src/app.js
+++ src/app.js
@@ -18,8 +18,8 @@ function displayTemperature(response) {
 }
 
 let apiKey = "c33d4a80d9533a8t8944b0aef1f6cbo2";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query={Seattle}&key=c33d4a80d9533a8t8944b0aef1f6cbo2&units=imperial";
+let city = "Seattle";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
 
 axios.get(apiUrl).then(displayTemperature);
 
@@ -27,3 +27,49 @@ axios.get(apiUrl).then(displayTemperature);
 //let highElement = Math.round(response.data.temperature.maximum);
 //let lowElement = Math.round(response.data.temperature.minimum);
 //highLow.innerHTML = `H: ${highElement}° L: ${lowElement}°`;
+// current date and time
+
+let now = new Date();
+let currentDate = document.querySelector("#currentDate");
+
+let date = now.getDate();
+let year = now.getFullYear();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "January",
+  "Febuary",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+currentDate.innerHTML = `${day} </br> 
+${date} ${month} ${year} </br>
+Time: ${hours}:${minutes}`;
